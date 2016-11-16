53c85227eb53e9a8eff6b00d24d2b2a6666f8870
hanzy <zsgs93315@163.com>
2016-11-16 17:09:42 +0800

    code Standardization 2016_11_16

--- hzy/logo_detection/make_voc_text.py
+++ hzy/logo_detection/make_voc_text.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 # this file to make 'voc-like' test.txt,train.txt,trainval.txt,val.txt
 # author hanzy 2016_11_10
@@ -9,25 +8,25 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("where_xml",
-    help="in which file u saving your xmls,example:/Users/mac/logo_detection/paste_img/Annotations")
+    help = "in which file u saving your xmls,example:/Users/mac/logo_detection/paste_img/Annotations")
 parser.add_argument("where_save_txt",
-    help="in which file u want to save your txts,example:/Users/mac/logo_detection/paste_img/ImageSets/Main")
+    help = "in which file u want to save your txts,example:/Users/mac/logo_detection/paste_img/ImageSets/Main")
 parser.add_argument("trainval_rate",
-    help="the trainval_rate is,example:0.5")
+    help = "the trainval_rate is,example:0.5")
 parser.add_argument("train_rate",
-    help="the train_rate is,example:0.5")
+    help = "the train_rate is,example:0.5")
 args = parser.parse_args()
 
-#where_xml = '/Users/mac/logo_detection/paste_img/Annotations'
+# where_xml = '/Users/mac/logo_detection/paste_img/Annotations'
 where_xml = args.where_xml
 
-#where_save_txt = '/Users/mac/logo_detection/paste_img/ImageSets/Main'
+# where_save_txt = '/Users/mac/logo_detection/paste_img/ImageSets/Main'
 where_save_txt = args.where_save_txt
 
-#trainval_rate = 0.5
+# trainval_rate = 0.5
 trainval_rate = float(args.trainval_rate)
 
-#train_rate = 0.5
+# train_rate = 0.5
 train_rate = float(args.train_rate)
 
 xml_paths = []
@@ -35,24 +34,46 @@ train = []
 val = []
 trainval = []
 test = []
-def get_xml_path (args,dirname,filenames):
+
+
+def get_xml_path (args, dirname, filenames):
+    """
+    Callback Function
+
+    @params:
+    args : the parameter of os.path.walk(where_xml, get_xml_path, None )
+    dirname : folder locations
+    filename : filename in folder locations
+    """
+
+    # get .xml files
     for filename in filenames:
         if filename[-3:] == 'xml':
             xml_paths.append(filename[:-4])
-def writepathtotxt(data_sets,path):
+
+
+def writepathtotxt(data_sets, path):
+    """
+
+    @params:
+    data_sets : the information lists
+    path : path to save data_sets
+    """
+
+    # use to save data_set infos
     txt_path = path
-    with open(txt_path,"w") as f:
+    with open(txt_path, "w") as f:
         for data_set in data_sets:
             f.write(data_set)
             f.write('\n')
 
 # get xml paths
-os.path.walk(where_xml, get_xml_path, None )
+os.path.walk(where_xml, get_xml_path, None)
 
 for xml_path in xml_paths:
     
     # xml use for test
-    if random.randint(0,100) > trainval_rate * 100:
+    if random.randint(0, 100) > trainval_rate * 100:
         test.append(xml_path)
         
     # xml use for trainval
@@ -60,7 +81,7 @@ for xml_path in xml_paths:
         trainval.append(xml_path)
         
         # xml use for val
-        if random.randint(0,100) > train_rate * 100:
+        if random.randint(0, 100) > train_rate * 100:
             val.append(xml_path)
         
         # xml use for train
@@ -68,13 +89,7 @@ for xml_path in xml_paths:
             train.append(xml_path)
 
 # save txt
-writepathtotxt(test,where_save_txt + '/test.txt')
-writepathtotxt(trainval,where_save_txt + '/trainval.txt')
-writepathtotxt(val,where_save_txt + '/val.txt')
-writepathtotxt(train,where_save_txt + '/train.txt')
-
-
-# In[ ]:
-
-
-
+writepathtotxt(test, where_save_txt + '/test.txt')
+writepathtotxt(trainval, where_save_txt + '/trainval.txt')
+writepathtotxt(val, where_save_txt + '/val.txt')
+writepathtotxt(train, where_save_txt + '/train.txt')
--- hzy/logo_detection/paste_img.py
+++ hzy/logo_detection/paste_img.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 # this file to make paste-logo images
 # author hanzy 2016_11_10
@@ -11,25 +10,25 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("img_savepath",
-    help="where u want to save your paste_images,example:/Users/mac/logo_detection/paste_img/new_img")
+    help = "where u want to save your paste_images,example:/Users/mac/logo_detection/paste_img/new_img")
 parser.add_argument("logo_dir",
-    help="where u saving your logo_examples,example:/Users/mac/logo_detection/paste_img/0_logo_dataset")
+    help = "where u saving your logo_examples,example:/Users/mac/logo_detection/paste_img/0_logo_dataset")
 parser.add_argument("bg_dir",
-    help="where u saving your bg_images: example:/Users/mac/logo_detection/dataset/FlickrLogos-32/classes/jpg/no-logo")
+    help = "where u saving your bg_images: example:/Users/mac/logo_detection/dataset/FlickrLogos-32/classes/jpg/no-logo")
 parser.add_argument("img_info_for_XML",
-    help="where u want to save your position_info.txt,example:/Users/mac/logo_detection/paste_img/position_info.txt")
+    help = "where u want to save your position_info.txt,example:/Users/mac/logo_detection/paste_img/position_info.txt")
 args = parser.parse_args()
 
-#img_savepath = '/Users/mac/logo_detection/paste_img/new_img'
+# img_savepath = '/Users/mac/logo_detection/paste_img/new_img'
 img_savepath = args.img_savepath
 
-#logo_dir = '/Users/mac/logo_detection/paste_img/0_logo_dataset'
+# logo_dir = '/Users/mac/logo_detection/paste_img/0_logo_dataset'
 logo_dir = args.logo_dir
 
-#bg_dir = '/Users/mac/logo_detection/dataset/FlickrLogos-32/classes/jpg/no-logo'
+# bg_dir = '/Users/mac/logo_detection/dataset/FlickrLogos-32/classes/jpg/no-logo'
 bg_dir = args.bg_dir
 
-#img_info_for_XML = '/Users/mac/logo_detection/paste_img/position_info.txt'
+# img_info_for_XML = '/Users/mac/logo_detection/paste_img/position_info.txt'
 img_info_for_XML = args.img_info_for_XML
 
 bg_file_paths = []
@@ -37,29 +36,72 @@ logo_dir_paths = []
 logo_paths = []
 position_info = []
 
-def getlogo_type_dir_path (args,dirname,filenames):
+def getlogo_type_dir_path(args, dirname, filenames):
+    """
+    Callback Function
+
+    @params:
+    args : the parameter of os.path.walk(logo_dir, getlogo_type_dir_path, None)
+    dirname : folder locations
+    filename : filename in folder locations
+    """
+
     for filename in filenames:
         if os.path.isdir(dirname + '/' + filename):
             logo_dir_paths.append(dirname + '/' + filename)
-def getbg_path (args,dirname,filenames):
+
+
+def getbg_path(args, dirname, filenames):
+    """
+    Callback Function
+
+    @params:
+    args : the parameter of os.path.walk(bg_dir, getbg_path, None)
+    dirname : folder locations
+    filename : filename in folder locations
+    """
+
     for filename in filenames:
         if filename[-3:] == 'jpg':
             bg_file_paths.append(dirname + '/' + filename)
-def getlogo_path (args,dirname,filenames):
+
+
+def getlogo_path (args, dirname, filenames):
+    """
+    Callback Function
+
+    @params:
+    args : the parameter of os.path.walk(logo_dir_path, getlogo_path, None)
+    dirname : folder locations
+    filename : filename in folder locations
+    """
+
     for filename in filenames:
         if os.path.isfile(dirname + '/' + filename):
             logo_paths.append(dirname + '/' + filename)  
+
+
 def writepathtotxt(data_sets):
+    """
+
+    @params:
+    data_sets : the information lists
+    """
+
+    # use to save data_set infos
     txt_path = img_info_for_XML
+
     with open(txt_path,"w") as f:
         for data_set in data_sets:
             f.write(data_set)
             f.write('\n')
+
+
 #get bg img path
-os.path.walk(bg_dir, getbg_path, None )
+os.path.walk(bg_dir, getbg_path, None)
 
 #get logo dir path
-os.path.walk(logo_dir, getlogo_type_dir_path, None )
+os.path.walk(logo_dir, getlogo_type_dir_path, None)
 
 #do
 for logo_dir_path in logo_dir_paths:
@@ -67,10 +109,10 @@ for logo_dir_path in logo_dir_paths:
     logo_paths = []
     
     #get one-type-logo's paths save in logo_paths
-    os.path.walk(logo_dir_path, getlogo_path, None )
+    os.path.walk(logo_dir_path, getlogo_path, None)
     while(1):
-        logo_id = random.randint(0,np.size(logo_paths)-1)
-        bg_file_id = random.randint(0,np.size(bg_file_paths)-1)
+        logo_id = random.randint(0, np.size(logo_paths)-1)
+        bg_file_id = random.randint(0, np.size(bg_file_paths)-1)
         try:
             #read img
             imglogo = Image.open(logo_paths[logo_id])
@@ -87,7 +129,7 @@ for logo_dir_path in logo_dir_paths:
                 
             #get logo img resize size by random in (1/16,1/2)
             while(1):
-                target_length = random.randint(my_length/16,my_length/2)
+                target_length = random.randint(my_length/16, my_length/2)
                 target_weigh = logo_weigh * target_length / logo_length
                 if target_length < bg_length and target_weigh < bg_weigh:
                     break;
@@ -96,16 +138,18 @@ for logo_dir_path in logo_dir_paths:
             mylogo = imglogo.resize((target_length, target_weigh), Image.ANTIALIAS)
 
             #which bg position target paste to 
-            point_position_x = random.randint(0,bg_length - target_length)
-            point_position_y = random.randint(0,bg_weigh - target_weigh)
+            point_position_x = random.randint(0, bg_length - target_length)
+            point_position_y = random.randint(0, bg_weigh - target_weigh)
 
             #paste logo
-            box = (point_position_x,point_position_y,                    point_position_x + target_length,point_position_y + target_weigh)
+            box = (point_position_x,point_position_y,point_position_x + target_length,point_position_y + target_weigh)
             imgbg.paste(mylogo, box)
             imgbg.save(img_savepath + '/' + logo_dir_path.split('/')[-1] + '_' + str(count) + '.jpg')
             count += 1
 
-            positioninfo = [logo_dir_path.split('/')[-1],'_',str(count),' ',logo_dir_path.split('/')[-1],' ',                   str(point_position_x),' ',str(point_position_y),' ',str(point_position_x + target_length),                   ' ',str(point_position_y + target_weigh)]
+            positioninfo = [logo_dir_path.split('/')[-1],'_',str(count),' ',logo_dir_path.split('/')[-1],' ',
+                str(point_position_x),' ',str(point_position_y),' ',str(point_position_x + target_length),' ',
+                str(point_position_y + target_weigh)]
             position_info.append(''.join(positioninfo))
             
             #every type of logo make 500 pages
@@ -119,9 +163,3 @@ for logo_dir_path in logo_dir_paths:
 writepathtotxt(position_info)
 
 print 'Done!'
-
-
-# In[ ]:
-
-
-
